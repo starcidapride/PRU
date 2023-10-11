e765361f5398c8604d505ecfbef8b32762fec001
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2023-10-11 20:08:06 +0700

    2nd

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : Singleton<PlayerManager>
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    public static T Instance { get; private set; }
+    public virtual void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+public class SingletonPersistent<T> : MonoBehaviour where T : Component
+{
+    public static T Instance { get; private set; }
+    public virtual void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
